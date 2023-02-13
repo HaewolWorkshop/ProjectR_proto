68bd488f6e2db656533ff6f0120d839fc8f9e94e
SBA\Bang <superbang175@gmail.com>
2023-02-14 02:24:58 +0900

    Chore : 젠킨스를 위한 빌드 스크립트 추가

---
+++ Assets/Builder.cs
@@ -0,0 +1,54 @@
+#if UNITY_EDITOR
+
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using UnityEditor.Build.Reporting;
+
+class Builder
+{
+    static string[] Scenes = FindEnabledEditorScenes();
+    static string GameName = PlayerSettings.productName;
+
+    [MenuItem("Build/빌드하기")]
+    public static void Build()
+    {
+        var dir = $"Builds/{PlayerSettings.productName}_{DateTime.Now.ToString("MMddHHmm")}/{PlayerSettings.productName}.exe";
+
+        GenericBuild(FindEnabledEditorScenes(), dir, BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows, BuildOptions.None);
+    }
+
+    public static void GenericBuild(string[] scenes, string target_dir, BuildTargetGroup build_group, BuildTarget build_target, BuildOptions build_options)
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(build_group, build_target);
+        var res = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options);
+
+        if (res.summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + res.summary.totalSize + " bytes");
+        }
+        else if (res.summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+
+    private static string[] FindEnabledEditorScenes()
+    {
+        var scenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled)
+            {
+                continue;
+            }
+
+            scenes.Add(scene.path);
+            Debug.Log(scene.path);
+        }
+        return scenes.ToArray();
+    }
+}
+#endif
